53b09461c82dc11ddff0ba1e7ad1dd1df18e6e42
imac <akashtejani2000@gmail.com>
2022-11-12 15:30:43 +0530

    main view initial commit

---
+++ lib/ui/main_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({Key? key}) : super(key: key);
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Center( child: Text("Main View"),)),
+    );
+  }
+}
